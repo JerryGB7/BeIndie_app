10a851a81672f0eace6c1f94b1d8dcf5c922007c
JerryGB7 <jerrygonzalezb7@gmail.com>
2023-08-21 18:35:07 -0700

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+
+
+export const metadata = {
+    title: "BeIndie",
+    description: 'A Chat Application For Indie Game Developers'
+}
+
+const inter = Inter({ subsets: ['latin']})
+
+export default function RootLayout({
+    children
+}:{
+    children: React.ReactNode
+}) {
+    return( 
+        <ClerkProvider>
+            <html lang='en'>
+                <body className={`{inter.className} bg-dark-1`}>
+                    <div className="w-full flex justify-center items-center min-h-screen">
+                        {children}
+                    </div>
+                </body>
+            </html>
+        </ClerkProvider>
+    )
+}
